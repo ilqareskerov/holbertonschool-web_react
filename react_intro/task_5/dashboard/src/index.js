692676a4a6c6d994b48c90a794745cd25cf88a1a
8933@holbertonstudents.com <ilqar.eskerov@mail.ru>
2024-07-24 16:56:14 +0400

    Ready

--- react_intro/task_5/dashboard/src/index.js
+++ react_intro/task_5/dashboard/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App/App';
 import Notifications from './Notifications/Notifications';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const root = ReactDOM.createRoot(document.getElementsByTagName('body')[0]);
 root.render(
-  <React.StrictMode>
-    <div className="root-notifications">
-      <Notifications />
-    </div>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <div className='root-notifications'>
+            <Notifications />
+        </div>
+        <App />
+    </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
